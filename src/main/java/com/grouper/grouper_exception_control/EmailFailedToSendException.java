65528c66abe0687c002f5095bbf11983dc0cabb0
Eze Kingsley Afamefula <kingsleyafamefula@gmail.com>
2023-08-21 18:05:23 +0100

    Grouper Social

--- src/main/java/com/grouper/grouper_exception_control/EmailFailedToSendException.java
+++ src/main/java/com/grouper/grouper_exception_control/EmailFailedToSendException.java
@@ -1,6 +1,6 @@
 package com.grouper.grouper_exception_control;
 
-public class EmailFailedToSendException extends Throwable {
+public class EmailFailedToSendException extends RuntimeException{
 
     public EmailFailedToSendException() {
         super("Email failed to send to the recipient");
